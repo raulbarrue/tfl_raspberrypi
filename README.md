2b58bfd3df9cf3ce54c19e39c0376f80a695992a
raulbarrue <raulbarrue@outlook.com>
2017-11-10 20:15:02 +0000

    Added infinite loop comment and fixed readme

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 In this project I'm building a multi colour led dashboard using a Raspberry Pi 3 and Python.
 
 Green light: Good Service
+
 Orange light: Minor Delays
+
 Red light: Severe Delays
+
 Blinking red light: Suspended
 
 ---------------------------------------------------
--- display.py
+++ display.py
@@ -2,7 +2,7 @@ from status import get_status # Importing the functions defined in the other scr
 import time
 import os
 
-while True:
+while True: # Infinite loop to constantly show the data (refreshed every 30 seconds)
 	all_lines = get_status() # Get the status of all lines
 	for lines in all_lines:
 		if all_lines[lines] == 10:
