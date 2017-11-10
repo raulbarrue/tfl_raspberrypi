602fd6ab065461ca8f3227742cdf24d948b8d5c2
raulbarrue <raulbarrue@outlook.com>
2017-11-10 19:47:09 +0000

    Added comments

--- display.py
+++ display.py
@@ -1,9 +1,9 @@
-from status import get_status
+from status import get_status # Importing the functions defined in the other script
 import time
 import os
 
 while True:
-	all_lines = get_status()
+	all_lines = get_status() # Get the status of all lines
 	for lines in all_lines:
 		if all_lines[lines] == 10:
 			print(lines + ": Good Service")
@@ -11,6 +11,6 @@ while True:
 			print(lines + ": Severe Delays")
 		elif all_lines[lines] == 9:
 			print(lines + ": Minor Delays")
-	time.sleep(30)
-	os.system("cls")
+	time.sleep(30) # Data will refresh every 30 seconds
+	os.system("cls") # This will clean the command screen
 
--- status.py
+++ status.py
@@ -3,7 +3,8 @@ import json
 from tfl_url import tfl_url
 	
 def get_status():
-	webpage = urlopen(tfl_url).read()
+	# This function will get the status for all the lines
+	webpage = urlopen(tfl_url).read() # tfl_url is where my API keys are stored
 	js = json.loads(webpage)
 	all_lines_status = {}
 
@@ -11,10 +12,10 @@ def get_status():
 		status_code = js[i]["lineStatuses"][0]["statusSeverity"]
 		line_name = js[i]["name"]
 		all_lines_status[line_name] = status_code
-		
 	return all_lines_status
 
 def get_status_line(line):
+	# This function will get the status for a given line
 	webpage = urlopen(tfl_url).read()
 	js = json.loads(webpage)
 	all_lines_status = {}
@@ -23,5 +24,4 @@ def get_status_line(line):
 		status_code = js[i]["lineStatuses"][0]["statusSeverity"]
 		line_name = js[i]["name"]
 		all_lines_status[line_name] = status_code
-		
 	return all_lines_status[line]
