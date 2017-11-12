b39c08ebdf69bdc3a66b5bb0ae00e20d266f0dc6
raulbarrue <raulbarrue@outlook.com>
2017-11-12 12:04:42 +0000

    Added new code status

--- display.py
+++ display.py
@@ -4,17 +4,21 @@ import os
 
 while True: # Infinite loop to constantly show the data (refreshed every 30 seconds)
 	all_lines = get_status() # Get the status of all lines
-	for lines in all_lines:
-		if all_lines[lines] == 10:
-			print(lines + ": Good Service")
-		elif all_lines[lines] == 6:
-			print(lines + ": Severe Delays")
-		elif all_lines[lines] == 9:
-			print(lines + ": Minor Delays")
-		elif all_lines[lines] == 5:
-			print(lines + ": Part Closure")
+	for line in all_lines:
+		if all_lines[line] == 10:
+			print(line + ": Good Service")
+		elif all_lines[line] == 6:
+			print(line + ": Severe Delays")
+		elif all_lines[line] == 9:
+			print(line + ": Minor Delays")
+		elif all_lines[line] == 5:
+			print(line + ": Part Closure")
+		elif all_lines[line] == 20:
+			print(line + ": Service Closed")
+		elif all_lines[line] == 3:
+			print(line + ": Part Suspended")
 		else:
-			print("Unknown Status")
+			print(line + ": Unknown Status")
 	time.sleep(30) # Data will refresh every 30 seconds
 	os.system("cls") # This will clean the command screen
 
